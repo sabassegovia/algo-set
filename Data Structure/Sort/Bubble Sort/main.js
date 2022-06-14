b62720b31588e50bdd8c97b726366543c78625e4
Sabas Segovia <sasegovia@ucdavis.edu>
2022-06-13 22:45:00 -0700

    bubble sort complete

--- Data Structure/Sort/Bubble Sort/main.js
+++ Data Structure/Sort/Bubble Sort/main.js
@@ -1,9 +1,32 @@
+// function bubble_sort(arr) {
+//     for (let i = 0; i < arr.length - 1; ++i)
+//         for (let j = 0; j < arr.length - 1 - i; ++j)
+//             if (arr[j] > arr[j + 1])
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+// }
+
+
+
+
+
+
 function bubble_sort(arr) {
-    for (let i = 0; i < arr.length - 1; ++i) 
-        for (let j = 0; j < arr.length - 1 - i; ++j) 
-            if (arr[j] > arr[j + 1])
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-} 
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        for (let i = 0; i < arr.length - 1; i++) {
+            for (let j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+
 
 function generateRandomArray(num, rangeFrom, rangeTo) {
     if (rangeTo < rangeFrom) {
